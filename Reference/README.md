c23e3edaf43a3705e521f961562e2c54e9c2f08e
jaimelay <jaimelay1997@gmail.com>
2019-03-18 23:06:23 -0300

    Reference Criado

---
+++ Reference/README.md
@@ -0,0 +1,2 @@
+# Reference
+Templates, Codes & Tricks.
---
+++ Reference/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main(void){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+    return 0;
+}
