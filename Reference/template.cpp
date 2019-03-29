1d2dbe72c7da7326c46126dd6d32adec9dda417f
Jaime Lay <jaimelay1997@gmail.com>
2019-03-29 19:54:28 -0300

    Update template.cpp

--- Reference/template.cpp
+++ Reference/template.cpp
@@ -7,10 +7,9 @@ typedef long long ll;
 using namespace std;
 
 int main(void){
-#ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-#endif
+    
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
