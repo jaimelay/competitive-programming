99653c2aba2bc854cea8064e04ac609fdca0f0c4
jaimelay <jaimelay1997@gmail.com>
2019-06-16 12:28:40 -0300

    template melhorado

--- Reference/template.cpp
+++ Reference/template.cpp
@@ -1,18 +1,28 @@
 #include <bits/stdc++.h>
+using namespace std;
 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/rope>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define DEBUG(x) cout << (#x) << " == " << x << endl;
 #define endl '\n'
 
 typedef long long ll;
+typedef long double ld;
 
-using namespace std;
+const ld PI = acos(-1.);
+const int MOD = 1e9+7;
 
 int main(void){
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
     
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
     return 0;
 }
