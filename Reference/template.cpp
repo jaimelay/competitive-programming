22f8d369339256ca253886353f3adb9609a14ff3
jaimelay <jaimelay1997@gmail.com>
2019-06-25 16:35:50 -0300

    template atualizado

--- Reference/template.cpp
+++ Reference/template.cpp
@@ -2,21 +2,37 @@
 
 using namespace std;
 
-#define DEBUG(x) cout << (#x) << " == " << x << endl;
+// FAST IO
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define endl '\n'
+// STL METHODS
+#define pb push_back
+#define mp make_pair
+#define st first
+#define nd second
+#define all(x) (x).begin(), (x).end()
+// MACROS
+#define FOR(x, n) for(int (x) = 0; (x) < (n); (x)++)
+#define FOR1(x, n) for(int (x) = 1; (x) <= (n); (x)++)
+// DEBUG MACROS
+#define DEBUG(x) cout << (#x) << " == " << x << endl;
 
 typedef long long ll;
 typedef long double ld;
 
-const ld PI = acos(-1.);
-const int MOD = 1e9+7;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const ll INFLL = 0x3f3f3f3f3f3f3f3fLL;
+const ld EPS = 1e-9, PI = acos(-1.0);
+
+int cmp_double(double a, double b, double eps = EPS){
+    return a + eps > b ? b + eps > a ? 0 : 1 : -1;
+}
 
 int main(void){
     // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
     
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    FAST_IO
 
     return 0;
 }
