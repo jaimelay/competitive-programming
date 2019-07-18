3042b51c1f3804feae850617664cc65419b7b545
jaimelay <jaimelay1997@gmail.com>
2019-07-18 00:54:30 -0300

    miscellanous reference

---
+++ Reference/miscellaneous/histogram_problem.cpp
@@ -0,0 +1,29 @@
+long long getMaxArea(vector<long long> &hist, long long n){
+    stack<long long> s;
+
+    long long max_area = 0; 
+    long long tp;  
+    long long area_with_top; 
+
+    long long i = 0;
+    while(i < n){
+		if(s.empty() || hist[s.top()] <= hist[i]){
+			s.push(i++);
+		}else{
+            tp = s.top();
+            s.pop();
+            area_with_top = hist[tp] * (s.empty() ? i : i - s.top() - 1);
+			if(max_area < area_with_top)
+				max_area = area_with_top;
+        }
+    }
+
+	while(!s.empty()){
+        tp = s.top();
+        s.pop();
+        area_with_top = hist[tp] * (s.empty() ? i : i - s.top() - 1);
+        if(max_area < area_with_top)
+            max_area = area_with_top;
+    }
+    return max_area;
+}
