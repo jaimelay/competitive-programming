217e74e12f009b72fcc5e9cdf3e66c3086a89169
jaimelay <jaimelay1997@gmail.com>
2019-08-08 20:59:24 -0300

    Cobertura Minima DFS

--- Reference/graphs/DFS.cpp
+++ Reference/graphs/DFS.cpp
@@ -1,15 +1,24 @@
-// Busca por profundidade - Depth First Search
-
-vector<int> adj[n];
+vector<int> g[n];
 int vis[n];
 
 int tam = 0;
 
-void DFS(int u){
-    vis[u] = true;
-    tam++;                  // O que fazer quando entrar no vertice.
-    for(auto v : adj[u])
-        if(!vis[v]) DFS(v);
+void DFS(int v){
+    vis[v] = 1;
+    for(auto u : g[v])
+        if(!vis[v]) DFS(u);
+}
+
+// Cobertura Minima
+// 0 = Não visitado, 1 = Visitado, 2 = Vertice da cobertura
+void DFS(int v){
+    vis[v] = 1;
+    for(auto u : g[v]){
+        if(vis[u] == 0){
+            DFS(u);
+            if(vis[u] == 1) vis[v] = 2;
+        }
+    }
 }
 
 // O vertice u esta conectado com dest?
