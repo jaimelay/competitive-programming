0dcc2cff6fa7805b31b621b081ba1ac9e1461cd3
jaimelay <jaimelay1997@gmail.com>
2019-11-20 16:59:32 -0300

    Modificações em segtree e segtree-lazy

--- Reference/data-structures/segtree-lazy.cpp
+++ Reference/data-structures/segtree-lazy.cpp
@@ -9,46 +9,44 @@ void build(int node, int left, int right){
         int mid = (left + right)/2;
         build(2*node, left, mid);
         build(2*node + 1, mid + 1, right);
-
-        st[node] = st[2*node] + st[2*node + 1];
+        st[node] = st[2*node] + st[2*node + 1]; // RMQ -> min/max; RSQ -> +
     }
 }
 
 void propagation(int node, int left, int right){
     if(lazy[node] != -1){
-        st[node] = lazy[node];
-        if(left != right){
-            lazy[2*node] = lazy[node];
-            lazy[2*node + 1] = lazy[node];
-        }
+        st[node] = lazy[node];  // RMQ  -> update: = lazy[node],                        increment: += lazy[node]
+                                // RSQ  -> update: = (right - left + 1) * lazy[node],   increment: += (right - left + 1) * lazy[node]
+        if(left != right)
+            lazy[2*node] = lazy[2*node + 1] = lazy[node]; // update: =, increment: +=
         lazy[node] = -1;
     }
 }
 
 void update(int node, int left, int right, int a, int b, int val){
     propagation(node, left, right);
-    if(a < left || b > right){
-        return;
-    }else if(left == right){
+
+    if(right < a || left > b) return;
+
+    if(left >= a && right <= b){
         lazy[node] = val;
         propagation(node, left, right);
     }else{
         int mid = (left + right)/2;
         update(2*node, left, mid, a, b, val);
         update(2*node + 1, mid + 1, right, a, b, val);
-
-        st[node] = st[2*node] + st[2*node + 1];
+        st[node] = st[2*node] + st[2*node + 1]; // RMQ -> min/max; RSQ -> +
     }
 }
 
 int query(int node, int left, int right, int a, int b){
     propagation(node, left, right);
-    if(a < left || b > right){
-        return 0;
-    }else if(left == right){
+
+    if(right < a || left > b) return 0; // RMQ -> INF, RSQ -> 0
+    if(left >= a && right <= b){
         return st[node];
     }else{
         int mid = (left + right)/2;
-        return query(2*node, left, mid, a, b) + query(2*node + 1, mid + 1, right, a, b);
+        return query(2*node, left, mid, a, b) + query(2*node + 1, mid + 1, right, a, b); // RMQ -> min/max; RSQ -> +
     }
 }
--- Reference/data-structures/segtree.cpp
+++ Reference/data-structures/segtree.cpp
@@ -9,35 +9,31 @@ void build(int node, int left, int right){
         int mid = (left + right)/2;
         build(2*node, left, mid);
         build(2*node + 1, mid + 1, right);
-
-        st[node] = st[2*node] * st[2*node + 1];
+        st[node] = st[2*node] + st[2*node + 1]; // RMQ -> min/max; RSQ -> +
     }
 }
 
 void update(int node, int left, int right, int idx, int val){
-    if(idx > right || idx < left){
-        return;
-    }else if(left == right){
-        a[idx] = val;
-        st[node] = val;
+    if(left == right){
+        a[idx] = st[node] = val; // update: =, increment: +=
     }else{
         int mid = (left + right)/2;
-        update(2*node, left, mid, idx, val);
-        update(2*node + 1, mid + 1, right, idx, val);
 
-        st[node] = st[2*node] * st[2*node + 1];
+        if(idx <= mid) update(2*node, left, mid, idx, val);
+        else update(2*node + 1, mid + 1, right, idx, val);
+
+        st[node] = st[2*node] + st[2*node + 1];
     }
 }
 
 int query(int node, int left, int right, int a, int b){
-    if(b < left || a > right){
-        return 1;
-    }else if(left >= a && right <= b){
+    if(b < left || a > right) return 0; // RMQ -> INF, RSQ -> 0
+    if(left >= a && right <= b){
         return st[node];
     }else{
         int mid = (left + right)/2;
         int e = query(2*node, left, mid, a, b);
         int d = query(2*node + 1, mid + 1, right, a, b);
-        return e * d;
+        return e + d; // RMQ -> min/max; RSQ -> +
     }
 }
