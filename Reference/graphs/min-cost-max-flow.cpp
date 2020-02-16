bea20d76a5e9bb2af959c80865411baa7bab9e81
jaimelay <jaimelay1997@gmail.com>
2020-02-16 11:19:19 -0300

    Adicionado dijkstra como opção para dense graphs

--- Reference/graphs/min-cost-max-flow.cpp
+++ Reference/graphs/min-cost-max-flow.cpp
@@ -40,7 +40,9 @@ void mcmf_bellman_ford(int s) {
     }
 }
 
-bool mcmf_dijkstra(int s, int t) {
+// Complexity: O(m*log(n))
+// FOR SPARSE GRAPHS USE THIS DIJKSTRA
+bool mcmf_dijkstra_sparse(int s, int t) {
     for (int i = 0; i < MAXN; i++) {
         dist[i] = INF;
         parent[i] = { -1, -1 };
@@ -76,7 +78,7 @@ bool mcmf_dijkstra(int s, int t) {
     }
 
 	for (int i = 0; i < MAXN; i++) {
-		if (phi[i] < INF and dist[i] < INF) {
+		if (phi[i] < INF && dist[i] < INF) {
 			phi[i] += dist[i];
         }
     }
@@ -84,11 +86,55 @@ bool mcmf_dijkstra(int s, int t) {
     return flag;
 }
 
+// Complexity: O(n^2 + m)
+// FOR DENSE GRAPHS USE THIS DIJKSTRA
+bool mcmf_dijkstra_dense(int s, int t) {
+    for (int i = 0; i < MAXN; i++) {
+        dist[i] = INF;
+        parent[i] = { -1, -1 };
+    }
+
+    vector<bool> vis(n, false);
+
+    d[s] = 0;
+    for (int i = 0; i < n; i++) {
+        int v = -1;
+        for (int j = 0; j < n; j++) {
+            if (!vis[j] && (v == -1 || dist[j] < dist[v]))
+                v = j;
+        }
+
+        if (dist[v] == INF) break;
+
+        vis[v] = true;
+        for (int i = 0; i < g[u].size(); i++) {
+            Edge e = g[u][i];
+            int v = e.to;
+
+            if (e.cap - e.flow <= 0) continue;
+
+            long long w = e.cost + phi[u] - phi[v];
+            if (dist[v] > d + w) {
+                dist[v] = d + w;
+                parent[v] = { u, i };
+            }
+        }
+    }
+
+	for (int i = 0; i < MAXN; i++) {
+		if (phi[i] < INF && dist[i] < INF) {
+			phi[i] += dist[i];
+        }
+    }
+
+    return parent[t] >= 0;
+}
+
 pair<long long, long long> mcmf(int s, int t) {
     long long min_cost = 0, max_flow = 0;
     mcmf_bellman_ford(s);
 
-    while (mcmf_dijkstra(s, t)) {
+    while (mcmf_dijkstra_sparse(s, t)) {
         long long flow = INF;
         for (int u = t; u != s; u = parent[u].first) {
             Edge e = g[parent[u].first][parent[u].second];
