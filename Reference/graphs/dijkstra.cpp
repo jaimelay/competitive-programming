9841fc9705b44b3235259518ef839723be92f215
jaimelay <jaimelay1997@gmail.com>
2020-02-16 11:41:30 -0300

    Adicionado dijkstra para dense graphs

--- Reference/graphs/dijkstra.cpp
+++ Reference/graphs/dijkstra.cpp
@@ -2,13 +2,14 @@
 
 const int INF = 0x3f3f3f3f;
 
-int dist[MAXN];
+int dist[MAXN], parent[MAXN];
 vector<pair<int, int>> g[MAXN];
 
-int dijkstra(int src, int dest){
-    for(int i = 1; i <= n; i++) dist[i] = INF;
+// Complexity: O(m * log(n))
+void dijkstra_sparse(int s){
+    for(int i = 0; i < n; i++) dist[i] = INF;
 
-    dist[src] = 0;
+    dist[s] = 0;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
     pq.push({ 0, src });
 
@@ -28,6 +29,36 @@ int dijkstra(int src, int dest){
             }
         }
     }
-    
-    return dist[dest];
+}
+
+// Complexity: O(n^2 + m)
+void dijkstra_dense(int s) {
+    for(int i = 0; i < n; i++) {
+        dist[i] = INF;
+        parent[i] = -1;
+    }
+
+    vector<bool> vis(n, false);
+
+    dist[s] = 0;
+    for (int i = 0; i < n; i++) {
+        int u = -1;
+        for (int j = 0; j < n; j++) {
+            if (!vis[j] && (u == -1 || dist[j] < dist[u]))
+                u = j;
+        }
+
+        if (dist[u] == INF) break;
+
+        vis[u] = true;
+        for (auto edge : g[u]) {
+            int v = edge.first;
+            int w = edge.second;
+
+            if (dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                parent[v] = u;
+            }
+        }
+    }
 }
