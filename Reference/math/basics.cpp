f51e1338c7520afac67146887ef26aabeab3279d
jaimelay <jaimelay1997@gmail.com>
2020-09-02 18:35:28 -0300

    chineses remainder

--- Reference/math/basics.cpp
+++ Reference/math/basics.cpp
@@ -47,38 +47,7 @@ long long inverseMod(long long a, long long mod = MOD) {
         return -1;
     } else {
         x = (x % mod + mod) % mod;
-        
+
         return x;
     }
-}
-
-long long chinese_remainder(vector<pair<long long, long long>> v) {
-    long long rem = v[0].first, mod = v[0].second;
-    long long ans = rem, m = mod;
-
-    for (int i = 1; i < (int)v.size(); i++) {
-        long long x, y;
-        rem = v[i].first, mod = v[i].second;
-        long long g = gcde(mod, m, x, y);
-
-        if ((ans - rem) % g != 0) {
-            return -1;
-        }
-
-        ans = ans + 1LL * (rem - ans) * (m / g) * y;
-        m = (mod / g) * (m / g) * g;
-        ans = (ans % m + m) % m;
-    }
-
-    if (ans == 0) {
-        long long _lcm = v[0].second;
-
-        for (int i = 1; i < (int)v.size(); i++) {
-            _lcm = lcm(_lcm, v[i].second);
-        }
-
-        return _lcm;
-    }
-
-    return ans;
 }
