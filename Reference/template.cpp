368cade5838437533446b8a5254eaa5aadbb2356
jaimelay <jaimelay1997@gmail.com>
2020-09-09 17:53:07 -0300

    changes

--- Reference/template.cpp
+++ Reference/template.cpp
@@ -2,15 +2,31 @@
 
 using namespace std;
 
-#define DEBUG(x) cout << (#x) << " == " << x << endl;
+#define dbg(x) cerr << endl << "DEBUG: (" << (#x) << " = " << x << ")" << endl;
+#define dbgArray(v) for (auto i : v) cerr << i << " "; cerr << endl;
 
-const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1e9+7;
 const long long INFLL = 0x3f3f3f3f3f3f3f3fLL;
-const long double EPS = 1e-9, PI = acos(-1.0);
+const long double EPS = 1e-9;
+const long double PI = acos(-1.0);
 
 int main(void){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        int n;
+
+        cin >> n;
+
+        vector<int> v(n);
+        for (auto &x : v) cin >> x;
+    }
 
     return 0;
 }
