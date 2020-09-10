afb110d465137dd47643c85ebf9e257ac99d1f3f
jaimelay <jaimelay1997@gmail.com>
2020-09-10 00:41:12 -0300

    changes

--- Reference/graphs/dinic.cpp
+++ Reference/graphs/dinic.cpp
@@ -1,5 +1,7 @@
-#define MAXN 10100
-#define INF 0x3f3f3f3f
+// Complexity:
+// Dinic - O(V^2 * E)
+// Bipartite Graph or Unit Flow - O(sqrt(V) * E)
+// Small flow - O(F * (V + E))
 
 struct Edge {
     int to;
@@ -8,26 +10,24 @@ struct Edge {
     int rev;
     int id;
 
-    Edge () {}
-
-    Edge (int to, int flow, int cap, int rev, int id) :
+    Edge(int to, int flow, int cap, int rev, int id = -1) :
         to(to), flow(flow), cap(cap), rev(rev), id(id) {}
 };
 
 vector<Edge> g[MAXN];
-int dist[MAXN], ptr[MAXN], ans[MAXN], src, sink;
+int dist[MAXN], ptr[MAXN];
 
-void add_edge(int from, int to, int cap, int id) {
+void addEdge(int from, int to, int cap, int id = 0) {
     g[from].push_back(Edge(to, 0, cap, g[to].size(), id));
     g[to].push_back(Edge(from, 0, 0, g[from].size() - 1, -id));
 }
 
-bool dinic_bfs() {
-    fill(dist, dist + MAXN, -1);
+bool dinicBFS(int s, int t) {
+    memset(dist, -1, sizeof(dist));
     queue<int> q;
 
-    dist[src] = 0;
-    q.push(src);
+    dist[s] = 0;
+    q.push(s);
 
     while (!q.empty()) {
         int u = q.front();
@@ -35,6 +35,7 @@ bool dinic_bfs() {
 
         for (Edge e : g[u]) {
             int v = e.to;
+
             if (dist[v] < 0 && e.flow < e.cap) {
                 dist[v] = dist[u] + 1;
                 q.push(v);
@@ -42,21 +43,27 @@ bool dinic_bfs() {
         }
     }
 
-    return dist[sink] >= 0;
+    return dist[t] >= 0;
 }
 
-int dinic_dfs(int u, int flow) {
-    if (u == sink) return flow;
+int dinicDFS(int s, int t, int flow) {
+    if (s == t) {
+        return flow;
+    }
 
-    for (int &i = ptr[u]; i < g[u].size(); i++) {
-        Edge &e = g[u][i];
+    for (int &i = ptr[s]; i < int(g[s].size()); i++) {
+        Edge &e = g[s][i];
+
         if (e.cap > e.flow) {
             int v = e.to;
-            if (dist[v] == dist[u] + 1) {
-                int tmp_flow = dinic_dfs(v, min(flow, e.cap - e.flow));
+
+            if (dist[v] == dist[s] + 1) {
+                int tmp_flow = dinicDFS(v, t, min(flow, e.cap - e.flow));
+
                 if (tmp_flow > 0) {
                     e.flow += tmp_flow;
                     g[v][e.rev].flow -= tmp_flow;
+
                     return tmp_flow;
                 }
             }
@@ -67,32 +74,36 @@ int dinic_dfs(int u, int flow) {
 }
 
 int dinic(int s, int t) {
-    src = s;
-    sink = t;
     int max_flow = 0;
 
-    while (dinic_bfs()) {
-        fill(ptr, ptr + MAXN, 0);                                           
-        while (int flow = dinic_dfs(src, INF)) max_flow += flow;
+    while (dinicBFS(s, t)) {
+        memset(ptr, 0, sizeof(ptr));
+
+        while (int flow = dinicDFS(s, t, INF)) {
+            max_flow += flow;
+        }
     }
 
     return max_flow;
 }
 
 bool cut[MAXN];
-void mincut_dfs(int u) {
+
+void minCutDFS(int u) {
     cut[u] = 1;
+
     for (auto x : g[u]) {
         if (x.cap > x.flow && !cut[x.to]) {
-            mincut_dfs(x.to);
+            minCutDFS(x.to);
         }
     }
 }
 
-vector<int> find_mincut() {
+vector<int> findMinCut() {
     vector<int> idx_edges;
+
     for (int i = 1; i <= n; i++) {
-        for (Edges e : g[i]) {
+        for (Edge e : g[i]) {
             if (cut[i] && !cut[e.to]) {
                 idx_edges.push_back(e.id);
             }
