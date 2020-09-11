c3faa06fbde0155583552dbda1d48488555cc15b
jaimelay <jaimelay1997@gmail.com>
2020-09-11 19:00:09 -0300

    changes

--- Reference/math/matrix_fast_exp.cpp
+++ Reference/math/matrix_fast_exp.cpp
@@ -1,13 +1,12 @@
 typedef vector<vector<long long>> matrix;
-long long mat_size;
 
-matrix mult(matrix a, matrix b, long long mod = MOD){
-    matrix c(mat_size, vector<long long>(mat_size));
+matrix matrixMultiplication(matrix a, matrix b){
+    matrix c(int(a.size()), vector<long long>(int(b[0].size())));
 
-    for (int i = 0; i < mat_size; i++) {
-        for (int j = 0; j < mat_size; j++) {
-            for (int k = 0; k < mat_size; k++) {
-                c[i][j] = (c[i][j] + a[i][k] * b[k][j]) % mod;
+    for (int i = 0; i < int(a.size()); i++) {
+        for (int j = 0; j < int(b[0].size()); j++) {
+            for (int k = 0; k < int(b.size()); k++) {
+                c[i][j] = (c[i][j] + a[i][k] * b[k][j]);
             }
         }
     }
@@ -15,16 +14,16 @@ matrix mult(matrix a, matrix b, long long mod = MOD){
     return c;
 }
 
-matrix pow(matrix base, long long n, long long mod = MOD){
+matrix matrixExponentiation(matrix base, long long n){
     if (n == 1) {
         return base;
     }
 
     if (n & 1) {
-        return mult(base, pow(base, n - 1));
+        return matrixMultiplication(base, matrixExponentiation(base, n - 1));
     }
 
-    matrix tmp = pow(base, n/2, mod);
+    matrix tmp = matrixExponentiation(base, n / 2);
     
-    return mult(tmp, tmp);
+    return matrixMultiplication(tmp, tmp);
 }
