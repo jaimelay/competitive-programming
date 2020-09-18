5ee0672ec13db6d1f4d5c774b683a879cc34c13f
jaimelay <jaimelay1997@gmail.com>
2020-09-18 00:13:34 -0300

    changes

--- Reference/data-structures/segtree.cpp
+++ Reference/data-structures/segtree.cpp
@@ -1,13 +1,12 @@
 // build: O(n), update: O(log(n)), query: O(log(n))
+// 0-indexed, query in the closed interval [l, r].
 struct SegmentTree {
     struct Node {
-        int val;
+        long long val;
     };
 
     int size;
     vector<Node> nodes;
-
-    Node NEUTRAL_ELEMENT = { INF };
 
     Node merge(Node a, Node b) {
         return { min(a.val, b.val) };
@@ -17,43 +16,41 @@ struct SegmentTree {
         return { v };
     }
 
-    void init(int n) {
-        size = 1;
-        while (size < n) size *= 2;
-        nodes.resize(2 * size);
-    }
-
-    void build(vector<int> &v, int ptr, int left, int right) {
-        if (right - left == 1) {
-            if (left < (int) v.size()) {
-                nodes[ptr] = single(v[left]);
-            }
-            return;
-        }
-
-        int mid = (right + left) / 2;
-        build(v, 2 * ptr + 1, left, mid);
-        build(v, 2 * ptr + 2, mid, right);
-        nodes[ptr] = merge(nodes[2 * ptr + 1], nodes[2 * ptr + 2]);
-    }
-
-    void build(vector<int> &v) {
+    SegmentTree(int n, vector<int> &v) {
+        size = n;
+        nodes.resize(4 * n);
         build(v, 0, 0, size);
     }
 
-    void update(int idx, int val, int ptr, int left, int right) {
-        if (right - left == 1) {
-            nodes[ptr] = single(val);
-            return;
-        }
-
-        int mid = (right + left) / 2;
-        if (idx < mid) {
-            update(idx, val, 2 * ptr + 1, left, mid);
+    void build(vector<int> &v, int ptr, int left, int right) {
+        if (right == left) {
+            if (left < (int) v.size()) {
+                nodes[ptr] = single(v[left]);
+            }
         } else {
-            update(idx, val, 2 * ptr + 2, mid, right);
+            int mid = (right + left) / 2;
+
+            build(v, 2 * ptr + 1, left, mid);
+            build(v, 2 * ptr + 2, mid + 1, right);
+
+            nodes[ptr] = merge(nodes[2 * ptr + 1], nodes[2 * ptr + 2]);
         }
-        nodes[ptr] = merge(nodes[2 * ptr + 1], nodes[2 * ptr + 2]);
+    }
+
+    void update(int idx, int val, int ptr, int left, int right) {
+        if (right == left) {
+            nodes[ptr] = single(val);
+        } else {
+            int mid = (right + left) / 2;
+
+            if (idx <= mid) {
+                update(idx, val, 2 * ptr + 1, left, mid);
+            } else {
+                update(idx, val, 2 * ptr + 2, mid + 1, right);
+            }
+
+            nodes[ptr] = merge(nodes[2 * ptr + 1], nodes[2 * ptr + 2]);
+        }
     }
 
     void update(int idx, int val) {
@@ -61,18 +58,21 @@ struct SegmentTree {
     }
 
     Node query(int l, int r, int ptr, int left, int right) {
-        if (left >= r || l >= right) {
-            return NEUTRAL_ELEMENT;
-        }
-
         if (left >= l && right <= r) {
             return nodes[ptr];
         }
 
         int mid = (right + left) / 2;
-        Node qry1 = query(l, r, 2 * ptr + 1, left, mid);
-        Node qry2 = query(l, r, 2 * ptr + 2, mid, right);
-        return merge(qry1, qry2);
+
+        if (r <= mid) {
+            return query(l, r, 2 * ptr + 1, left, mid);
+        } else if (l > mid) {
+            return query(l, r, 2 * ptr + 2, mid + 1, right);
+        } else {
+            Node qry1 = query(l, r, 2 * ptr + 1, left, mid);
+            Node qry2 = query(l, r, 2 * ptr + 2, mid + 1, right);
+            return merge(qry1, qry2);
+        }
     }
 
     Node query(int l, int r) {
