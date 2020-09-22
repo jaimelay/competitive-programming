b3e4b115e5b3f5a9272e2e95b09b6271c1ec311c
jaimelay <jaimelay1997@gmail.com>
2020-09-22 14:47:33 -0300

    .

--- Reference/geometry/Point2D.cpp
+++ Reference/geometry/Point2D.cpp
@@ -5,20 +5,20 @@ template <class T> int sgn(T x) {
 
 template <class T> struct Point2D { 
 	typedef Point2D P;
-    T x, y;
+	T x, y;
 
-    P(T x = 0, T y = 0) : x(x), y(y) {}
+	P(T x = 0, T y = 0) : x(x), y(y) {}
 
 	bool operator < (P p) { return tie(x,y) < tie(p.x, p.y); }
 	bool operator == (P p) { return tie(x,y) == tie(p.x, p.y); }
-    bool operator != (P p) { return !(tie(x,y) == tie(p.x, p.y)); }
-    P operator - (P p) { return P(x - p.x, y - p.y); }
-    P operator + (P p) { return P(x + p.x, y + p.y); }
-    P operator * (T k) { return P(k * x, k * y); }
-    P operator / (T k) { return P(x / k, y / k); }
+	bool operator != (P p) { return !(tie(x,y) == tie(p.x, p.y)); }
+	P operator - (P p) { return P(x - p.x, y - p.y); }
+	P operator + (P p) { return P(x + p.x, y + p.y); }
+	P operator * (T k) { return P(k * x, k * y); }
+	P operator / (T k) { return P(x / k, y / k); }
 
-    T dot(P p) { return x * p.x + y * p.y; }
-    T cross(P p) { return x * p.y - y * p.x; }
+	T dot(P p) { return x * p.x + y * p.y; }
+	T cross(P p) { return x * p.y - y * p.x; }
 	T cross(P a, P b) const { return (a - *this).cross(b - *this); }
 	T dist2() const { return x * x + y * y; }
 	double dist() const { return sqrt((double)dist2()); }
@@ -26,7 +26,7 @@ template <class T> struct Point2D {
 	P unit() const { return *this/dist(); } // makes dist()=1
 	P perp() const { return P(-y, x); } // rotates +90 degrees
 	P normal() const { return perp().unit(); }
-	
+
 	// returns point rotated 'a' radians ccw around the origin
 	P rotate(double a) const {
 		return P(x * cos(a) - y * sin(a), x * sin(a) + y * cos(a));
@@ -44,5 +44,5 @@ template <class T> struct Point2D {
 
 // Return -1: a < b, 0: a == b, 1: a > b
 int compareFloats(double a, double b, double eps = EPS) {
-  return (a > b + eps) - (a < b - eps);
+	return (a > b + eps) - (a < b - eps);
 }
