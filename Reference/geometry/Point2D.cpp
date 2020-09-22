00fad315b87ff23d4f9f58f6e0bb56a8b2552edb
jaimelay <jaimelay1997@gmail.com>
2020-09-22 20:21:43 -0300

    changes

--- Reference/geometry/Point2D.cpp
+++ Reference/geometry/Point2D.cpp
@@ -4,40 +4,39 @@ template <class T> int sgn(T x) {
 }
 
 template <class T> struct Point2D { 
-	typedef Point2D P;
 	T x, y;
 
-	P(T x = 0, T y = 0) : x(x), y(y) {}
+	Point2D(T x = 0, T y = 0) : x(x), y(y) {}
 
-	bool operator < (P p) { return tie(x,y) < tie(p.x, p.y); }
-	bool operator == (P p) { return tie(x,y) == tie(p.x, p.y); }
-	bool operator != (P p) { return !(tie(x,y) == tie(p.x, p.y)); }
-	P operator - (P p) { return P(x - p.x, y - p.y); }
-	P operator + (P p) { return P(x + p.x, y + p.y); }
-	P operator * (T k) { return P(k * x, k * y); }
-	P operator / (T k) { return P(x / k, y / k); }
+	bool operator < (Point2D p) { return tie(x,y) < tie(p.x, p.y); }
+	bool operator == (Point2D p) { return tie(x,y) == tie(p.x, p.y); }
+	bool operator != (Point2D p) { return !(tie(x,y) == tie(p.x, p.y)); }
+	Point2D operator - (Point2D p) { return Point2D(x - p.x, y - p.y); }
+	Point2D operator + (Point2D p) { return Point2D(x + p.x, y + p.y); }
+	Point2D operator * (T k) { return Point2D(k * x, k * y); }
+	Point2D operator / (T k) { return Point2D(x / k, y / k); }
 
-	T dot(P p) { return x * p.x + y * p.y; }
-	T cross(P p) { return x * p.y - y * p.x; }
-	T cross(P a, P b) const { return (a - *this).cross(b - *this); }
+	T dot(Point2D p) { return x * p.x + y * p.y; }
+	T cross(Point2D p) { return x * p.y - y * p.x; }
+	T cross(Point2D a, Point2D b) const { return (a - *this).cross(b - *this); }
 	T dist2() const { return x * x + y * y; }
 	double dist() const { return sqrt((double)dist2()); }
 	double angle() const { return atan2(y, x); } // angle to x-axis in interval [-pi, pi]
-	P unit() const { return *this/dist(); } // makes dist()=1
-	P perp() const { return P(-y, x); } // rotates +90 degrees
-	P normal() const { return perp().unit(); }
+	Point2D unit() const { return *this / dist(); } // makes dist()=1
+	Point2D perp() const { return Point2D(-y, x); } // rotates +90 degrees
+	Point2D normal() const { return perp().unit(); }
 
 	// returns point rotated 'a' radians ccw around the origin
-	P rotate(double a) const {
-		return P(x * cos(a) - y * sin(a), x * sin(a) + y * cos(a));
+	Point2D rotate(double a) const {
+		return Point2D(x * cos(a) - y * sin(a), x * sin(a) + y * cos(a));
 	}
 
 	// Returns true if this point lies on the line segment from point 's' to point'e'
-	bool onSegment(P s, P e) {
+	bool onSegment(Point2D s, Point2D e) {
 		return (*this).cross(s, e) == 0 && (s - *this).dot(e - *this) <= 0;
 	}
 
-	friend ostream& operator << (ostream& os, P p) {
+	friend ostream& operator << (ostream& os, Point2D p) {
 		return os << "(" << p.x << "," << p.y << ")"; 
 	}
 };
