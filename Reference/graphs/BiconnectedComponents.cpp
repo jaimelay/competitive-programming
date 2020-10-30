7ff5281f3eb2a038c23ac0a99aff60e7ad40e801
jaimelay <jaimelay1997@gmail.com>
2020-10-30 02:31:01 -0300

    update bcc algorithm

--- Reference/graphs/BiconnectedComponents.cpp
+++ Reference/graphs/BiconnectedComponents.cpp
@@ -1,13 +1,12 @@
 // Finds all Biconnected Components in an undirected graph using Tarjan's Algorithm
-// An edge which is not in a component && (bccs[i].size() > 2) is a bridge, i.e., not part of any cycle.
+// An edge which is not in a component (bccs[i].size() > 2) is a bridge, i.e., not part of any cycle.
 // Complexity: O(n + m)
 struct BCC {
     int n, timer, bccnum = 0;
-    vector<set<int>> bccs;
-    vector<vector<int>> block_cut;
-    vector<int> in, low, vis;
     stack<pair<int, int>> stck;
-    vector<int> articulations;
+    vector<int> in, low, vis, what_block;
+    vector<bool> is_articulation;
+    vector<vector<int>> block_cut_tree, bccs;
 
     BCC(int n) : n(n) {
         bccnum = 0;
@@ -16,8 +15,9 @@ struct BCC {
         in.resize(n);
         low.resize(n);
         vis.resize(n);
-        block_cut.resize(n);
-        articulations.clear();
+        what_block.resize(n);
+        is_articulation.resize(n);
+        block_cut_tree.resize(2 * n);
         while (!stck.empty()) stck.pop();
     }
 
@@ -34,7 +34,8 @@ struct BCC {
 
                     if (low[v] >= in[u]) {
                         if (in[u] > 1 || in[v] > 2) {
-                            articulations.push_back(u);
+                            is_articulation[u] = true;
+                            bccs[bccnum].push_back(u);
                         }
 
                         while (true) {
@@ -43,8 +44,11 @@ struct BCC {
 
                             int a = edge.first, b = edge.second;
                             
-                            bccs[bccnum].insert(a);
-                            bccs[bccnum].insert(b);
+                            what_block[a] = bccnum;
+                            bccs[bccnum].push_back(a);
+
+                            what_block[b] = bccnum;
+                            bccs[bccnum].push_back(b);
 
                             if (a == v && b == u) {
                                 break;
@@ -61,24 +65,31 @@ struct BCC {
         }
     }
 
-    void buildBlockCutTree() {
-        int nodes = bccnum;
-
-        for (auto art : articulations) {
-            for (int i = 0; i < bccnum; i++) {
-                if (binary_search(bccs[i].begin(), bccs[i].end(), art)) {
-                    block_cut[nodes].push_back(i);
-                    block_cut[i].push_back(nodes);
-                }
+    void findBCCs() {
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                DFS(i);
             }
-            nodes++;
         }
     }
 
-    void findBCCs() {
-        for (int i = 1 ; i <= n; ++i ) {
-            if (!vis[i]) {
-                DFS(i);
+    // 0 ... bccnum - 1 are blocks.
+    // bccnum ... bccnum + number of articulations points are cut points.
+    void buildBlockCutTree() {
+        int cuts = bccnum;
+
+        for (int v = 0; v < n; v++) {
+            if (is_articulation[v]) {
+                what_block[v] = cuts++;
+            }
+        }
+
+        for (int blck = 0; blck < bccnum; blck++) {
+            for (auto v : bccs[blck]) {
+                if (is_articulation[v]) {
+                    block_cut_tree[blck].push_back(what_block[v]);
+                    block_cut_tree[what_block[v]].push_back(blck);
+                }
             }
         }
     }
