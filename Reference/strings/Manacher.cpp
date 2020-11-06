37fdcf0734155db4e46ee72971f587dc997ffec9
jaimelay <jaimelay1997@gmail.com>
2020-11-06 00:23:49 -0300

    Add Manacher algorithm

---
+++ Reference/strings/Manacher.cpp
@@ -0,0 +1,73 @@
+// Returns all palindromes substrings, longest palindrome substring, if some range is palindrome.
+// Complexity: O(n)
+struct Manacher {
+    int n;
+    string s;
+    vector<int> odd, even, p;
+    vector<pair<int, int>> palindromes;
+
+    Manacher(const string &s) {
+        this->s = s;
+        this->n = (int)s.size();
+        this->odd.resize(n);
+        this->even.resize(n);
+        this->p.resize(2 * n - 1);
+        preprocess();
+    }
+
+    void preprocess() {
+        for (int i = 0, l = 0, r = -1; i < n; i++) {
+            int k = (i > r) ? 1 : min(odd[l + r - i], r - i + 1);
+            while (0 <= i - k && i + k < n && s[i - k] == s[i + k]) {
+                k++;
+            }
+            odd[i] = k--;
+            if (i + k > r) {
+                l = i - k;
+                r = i + k;
+            }
+        }
+
+        for (int i = 0, l = 0, r = -1; i < n; i++) {
+            int k = (i > r) ? 0 : min(even[l + r - i + 1], r - i + 1);
+            while (0 <= i - k - 1 && i + k < n && s[i - k - 1] == s[i + k]) {
+                k++;
+            }
+            even[i] = k--;
+            if (i + k > r) {
+                l = i - k - 1;
+                r = i + k ;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (odd[i]) {
+                palindromes.push_back({ i - odd[i] + 1, i + odd[i] - 1 });
+            }
+
+            if (even[i]) {
+                palindromes.push_back({ i - even[i], i + even[i] - 1 });
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            p[2 * i] = 2 * odd[i] - 1;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            p[2 * i + 1] = 2 * even[i + 1];
+        }
+    }
+
+    bool isPalindrome(int l, int r) {
+        return p[l + r] >= r - l + 1;
+    }
+
+    int longestPalindromeSize() {
+        return *max_element(p.begin(), p.end());
+    }
+
+    vector<pair<int, int>> getAllPalindromesIndexes() {
+        return palindromes;
+    }
+};
