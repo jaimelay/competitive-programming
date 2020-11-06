c0e1faed5146a88ed63cd2a03bc43ae81429cbe5
jaimelay <jaimelay1997@gmail.com>
2020-11-05 22:38:39 -0300

    update template

--- Reference/templates/normal.cpp
+++ Reference/templates/normal.cpp
@@ -2,8 +2,40 @@
 
 using namespace std;
 
-#define dbg(x) cerr << endl << "DEBUG: (" << (#x) << " = " << x << ")" << endl;
-#define dbgArray(v) for (auto i : v) cerr << i << " "; cerr << endl;
+string to_string(string s) { return '"' + s + '"'; }
+string to_string(const char* s) { return to_string((string) s); }
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "\n{";
+    for (const auto &x : v) {
+        if (!first) res += ", ";
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+ 
+void debug_out() { cerr << endl; }
+ 
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+ 
+#ifdef LOCAL
+    #define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+    #define debug(...) 42
+#endif
 
 const int INF = 0x3f3f3f3f;
 const int MOD = 1e9+7;
