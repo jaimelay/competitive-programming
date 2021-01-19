2f128112c478574e81cbad63cf8aea9f63d9d8e0
jaimelay <jaimelay1997@gmail.com>
2021-01-19 15:59:09 -0300

    fix: sieve algorithm

--- Reference/math/Sieve.cpp
+++ Reference/math/Sieve.cpp
@@ -1,18 +1,19 @@
 // Complexity: O(n * log(log(n)))
-
-vector<int> sieve(int n) {
-    vector<int> primes;
+vector<long long> sieve(int n = 10'000'000) {
+    vector<long long> primes;
     vector<bool> is_prime(n + 1, true);
-    is_prime[0] = false;
-    is_prime[1] = false;
+    is_prime[0] = false, is_prime[1] = false;
 
     for (int i = 2; i * i <= n; i++) {
         if (is_prime[i]) {
-            primes.push_back(i);
             for (int j = i * i; j <= n; j += i) {
                 is_prime[j] = false;
             }
         }
+    }
+
+    for (int i = 2; i <= n; i++) {
+        if (is_prime[i]) primes.push_back(i);
     }
 
     return primes;
